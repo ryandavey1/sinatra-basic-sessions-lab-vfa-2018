f9700f4413c98f9d3bc0e7e928b396f4ef5c9958
Ryan Davey <ryancdavey@icloud.com>
2018-07-01 04:58:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end 
+  
+  get '/' do 
+    @session = session
+    erb :index
+  end
 end
