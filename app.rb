2480c213da1340b580073921d775a2de1899dd56
Ryan Davey <ryancdavey@icloud.com>
2018-07-01 05:00:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,11 +4,16 @@ class App < Sinatra::Base
   
   configure do
     enable :sessions
-    set :session_secret, "secret"
-  end 
-  
-  get '/' do 
-    @session = session
+    set :session_secret, 'flatiron'
+  end
+
+  get '/' do
     erb :index
+  end
+
+  post '/checkout' do 
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
   end
 end
